4cda4fec627879c6c6090855c1288eda5d0bfe50
Bilel Taboubi <80677927+bibolil@users.noreply.github.com>
2024-09-20 14:35:31 +0100

    Update kick.py

--- src/streamlink/plugins/kick.py
+++ src/streamlink/plugins/kick.py
@@ -1,204 +1,161 @@
 """
-$description Global live-streaming and video hosting social platform owned by Kick Streaming Pty Ltd.
+$description Kick, a gaming livestreaming platform
 $url kick.com
 $type live, vod
-$metadata id
-$metadata author
-$metadata category
-$metadata title
 """
 
+import os
 import re
-from ssl import OP_NO_TICKET
+import logging
 
 from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import validate
-from streamlink.session.http import SSLContextAdapter
-from streamlink.stream.hls import HLSStream
+from streamlink.stream import HLSStream
+from streamlink.utils.parse import parse_json
+from streamlink.exceptions import PluginError
+from seleniumwire import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.common.by import By
 
-
-class KickAdapter(SSLContextAdapter):
-    def get_ssl_context(self):
-        ctx = super().get_ssl_context()
-        ctx.options &= ~OP_NO_TICKET
-
-        return ctx
+log = logging.getLogger(__name__)
 
 
 @pluginmatcher(
+    re.compile(
+        # https://github.com/yt-dlp/yt-dlp/blob/9b7a48abd1b187eae1e3f6c9839c47d43ccec00b/yt_dlp/extractor/kick.py#LL33-L33C111
+        r"https?://(?:www\.)?kick\.com/(?!(?:video|categories|search|auth)(?:[/?#]|$))(?P<channel>[\w_-]+)$",
+    ),
     name="live",
-    pattern=re.compile(r"https?://(?:\w+\.)?kick\.com/(?!video/)(?P<channel>[^/?]+)$"),
 )
 @pluginmatcher(
+    re.compile(
+        # https://github.com/yt-dlp/yt-dlp/blob/2d5cae9636714ff922d28c548c349d5f2b48f317/yt_dlp/extractor/kick.py#LL84C18-L84C104
+        r"https?://(?:www\.)?kick\.com/video/(?P<video_id>[\da-f]{8}-(?:[\da-f]{4}-){3}[\da-f]{12})",
+    ),
     name="vod",
-    pattern=re.compile(r"https?://(?:\w+\.)?kick\.com/video/(?P<vod>[^/?]+)"),
 )
 @pluginmatcher(
+    re.compile(
+        r"https?://(?:www\.)?kick\.com/(?!(?:video|categories|search|auth)(?:[/?#]|$))(?P<channel>[\w_-]+)\?clip=(?P<clip_id>[\w_]+)$",
+    ),
     name="clip",
-    pattern=re.compile(r"https?://(?:\w+\.)?kick\.com/(?!video/)(?P<channel>[^/?]+)\?clip=(?P<clip>[^&]+)$"),
 )
 class Kick(Plugin):
-    _URL_TOKEN = "https://kick.com/"
-    _URL_API_LIVESTREAM = "https://kick.com/api/v2/channels/{channel}/livestream"
-    _URL_API_VOD = "https://kick.com/api/v1/video/{vod}"
-    _URL_API_CLIP = "https://kick.com/api/v2/clips/{clip}"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.session.http.mount("https://kick.com/", KickAdapter())
-
-    def _get_token(self):
-        res = self.session.http.get(self._URL_TOKEN, raise_for_status=False)
-        return res.cookies.get("XSRF-TOKEN", "")
-
-    def _get_api_headers(self):
-        token = self._get_token()
-
-        return {
-            "Accept": "application/json",
-            "Accept-Language": "en-US",
-            "Referer": self.url,
-            "Authorization": f"Bearer {token}",
-        }
-
-    def _get_streams_live(self):
-        self.author = self.match["channel"]
-
-        data = self.session.http.get(
-            self._URL_API_LIVESTREAM.format(channel=self.author),
-            acceptable_status=(200, 404),
-            headers=self._get_api_headers(),
-            schema=validate.Schema(
-                validate.parse_json(),
-                validate.any(
-                    validate.all(
-                        {"message": str},
-                        validate.transform(lambda _: None),
-                    ),
-                    validate.all(
-                        {"data": None},
-                        validate.transform(lambda _: None),
-                    ),
-                    validate.all(
-                        {
-                            "data": {
-                                "playback_url": validate.url(path=validate.endswith(".m3u8")),
-                                "id": int,
-                                "category": {"name": str},
-                                "session_title": str,
-                            },
-                        },
-                        validate.get("data"),
-                        validate.union_get(
-                            "playback_url",
-                            "id",
-                            ("category", "name"),
-                            "session_title",
-                        ),
-                    ),
-                ),
-            ),
-        )
-        if not data:
-            return
-
-        hls_url, self.id, self.category, self.title = data
-
-        return HLSStream.parse_variant_playlist(self.session, hls_url)
-
-    def _get_streams_vod(self):
-        self.id = self.match["vod"]
-
-        data = self.session.http.get(
-            self._URL_API_VOD.format(vod=self.id),
-            acceptable_status=(200, 404),
-            headers=self._get_api_headers(),
-            schema=validate.Schema(
-                validate.parse_json(),
-                validate.any(
-                    validate.all(
-                        {"message": str},
-                        validate.transform(lambda _: None),
-                    ),
-                    validate.all(
-                        {
-                            "source": validate.url(path=validate.endswith(".m3u8")),
-                            "livestream": {
-                                "session_title": str,
-                                "channel": {
-                                    "user": {
-                                        "username": str,
-                                    },
-                                },
-                            },
-                        },
-                        validate.union_get(
-                            "source",
-                            ("livestream", "channel", "user", "username"),
-                            ("livestream", "session_title"),
-                        ),
-                    ),
-                ),
-            ),
-        )
-        if not data:
-            return
-
-        hls_url, self.author, self.title = data
-
-        return HLSStream.parse_variant_playlist(self.session, hls_url)
-
-    def _get_streams_clip(self):
-        self.id = self.match["clip"]
-        self.author = self.match["channel"]
-
-        data = self.session.http.get(
-            self._URL_API_CLIP.format(clip=self.id),
-            acceptable_status=(200, 404),
-            headers=self._get_api_headers(),
-            schema=validate.Schema(
-                validate.parse_json(),
-                validate.any(
-                    validate.all(
-                        {"message": str},
-                        validate.transform(lambda _: None),
-                    ),
-                    validate.all(
-                        {"clip": None},
-                        validate.transform(lambda _: None),
-                    ),
-                    validate.all(
-                        {
-                            "clip": {
-                                "clip_url": validate.url(path=validate.endswith(".m3u8")),
-                                "category": {"name": str},
-                                "title": str,
-                            },
-                        },
-                        validate.get("clip"),
-                        validate.union_get(
-                            "clip_url",
-                            ("category", "name"),
-                            "title",
-                        ),
-                    ),
-                ),
-            ),
-        )
-        if not data:
-            return
-
-        hls_url, self.category, self.title = data
-
-        return {"clip": HLSStream(self.session, hls_url)}
-
     def _get_streams(self):
-        if self.matches["live"]:
-            return self._get_streams_live()
-        if self.matches["vod"]:
-            return self._get_streams_vod()
-        if self.matches["clip"]:
-            return self._get_streams_clip()
+        API_BASE_URL = "https://kick.com/api"
+
+        _LIVE_SCHEMA = validate.Schema(
+            validate.parse_json(),
+            {
+                "playback_url": validate.url(path=validate.endswith(".m3u8")),
+                "livestream": {
+                    "is_live": True,
+                    "id": int,
+                    "session_title": str,
+                    "categories": [{"name": str}],
+                },
+                "user": {"username": str},
+            },
+            validate.union_get(
+                "playback_url",
+                ("livestream", "id"),
+                ("user", "username"),
+                ("livestream", "session_title"),
+                ("livestream", "categories", 0, "name"),
+            ),
+        )
+
+        _VIDEO_SCHEMA = validate.Schema(
+            validate.parse_json(),
+            {
+                "source": validate.url(path=validate.endswith(".m3u8")),
+                "id": int,
+                "livestream": {
+                    "channel": {"user": {"username": str}},
+                    "session_title": str,
+                    "categories": [{"name": str}],
+                },
+            },
+            validate.union_get(
+                "source",
+                "id",
+                ("livestream", "channel", "user", "username"),
+                ("livestream", "session_title"),
+                ("livestream", "categories", 0, "name"),
+            ),
+        )
+
+        _CLIP_SCHEMA = validate.Schema(
+            validate.parse_json(),
+            {
+                "clip": {
+                    "video_url": validate.url(path=validate.endswith(".m3u8")),
+                    "id": str,
+                    "channel": {"username": str},
+                    "title": str,
+                    "category": {"name": str},
+                },
+            },
+            validate.union_get(
+                ("clip", "video_url"),
+                ("clip", "id"),
+                ("clip", "channel", "username"),
+                ("clip", "title"),
+                ("clip", "category", "name"),
+            ),
+        )
+
+        live, vod, clip = (
+            self.matches["live"],
+            self.matches["vod"],
+            self.matches["clip"],
+        )
+
+        try:
+            options = webdriver.FirefoxOptions()
+            options.add_argument('--headless')
+            options.add_argument("--no-sandbox")
+            proxy=os.getenv("PROXY")
+            proxy_settings = {
+            'proxy': {
+                'http': 'http://'+proxy,
+                'https': 'https://'+proxy,
+                'no_proxy': 'localhost:127.0.0.1',
+                }
+             }
+            service = FirefoxService(executable_path="/home/site/wwwroot/geckodriver")
+            driver = webdriver.Firefox(service=service, options=options,seleniumwire_options=proxy_settings)
+            url = "{0}/{1}/{2}".format(
+                API_BASE_URL,
+                *(
+                    ["v1/channels", self.match["channel"]]
+                    if live
+                    else (
+                        ["v1/video", self.match["video_id"]]
+                        if vod
+                        else ["v2/clips", self.match["clip_id"]]
+                    )
+                )
+            )
+            driver.get(url)
+            page_source = driver.find_element(By.TAG_NAME, "body").text
+            url, self.id, self.author, self.title, self.category = (
+                _LIVE_SCHEMA if live else (_VIDEO_SCHEMA if vod else _CLIP_SCHEMA)
+            ).validate(page_source)
+
+        except (PluginError, TypeError) as err:
+            log.debug(err)
+            return
+        
+        finally:
+            driver.quit()
+
+        if live or vod:
+            yield from HLSStream.parse_variant_playlist(self.session, url).items()
+        elif (
+            clip and self.author.casefold() == self.match["channel"].casefold()
+        ):  
+            yield "source", HLSStream(self.session, url)
 
 
 __plugin__ = Kick
